6c4291b17720ee5d361c14cb3ae6f3e708e5ef93
me_madness <p_rangel@abv.bg>
2025-01-25 02:02:22 +0100

    feat: I need to add the rest of my info

--- rp_portfolio/main/urls.py
+++ rp_portfolio/main/urls.py
@@ -4,8 +4,9 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),    
     path("home", views.home, name="home"),    
-    path("aboutMe", views.home, name="aboutMe"),    
-    path("education", views.home, name="education"),    
+    path("about_me", views.aboutMe, name="aboutMe"),    
+    path("education", views.education, name="education"),    
+    path("projects", views.projects, name="projects"), 
     path("contact", views.contact, name="contact"), 
-    path("project/<int:id>/", views.project, name="project"),   
+    path("project_view/<int:id>/", views.projects, name="project_view"),   
 ]
--- rp_portfolio/main/views.py
+++ rp_portfolio/main/views.py
@@ -2,20 +2,24 @@ from django.shortcuts import render, get_object_or_404
 from .models import Project, Tag
 
 # Create your views here.
+
 def home(request):
-    projects = Project.objects.all()
-    tags = Tag.objects.all()
-    return render(request, "home.html", {"projects": projects, "tags": tags})
-
-def contact(request):
-    return render(request, "contact.html")
-
-def education(request):
-    return render(request, "education.html")
+    return render(request, "home.html")
 
 def aboutMe(request):
     return render(request, "about_me.html")
 
-def project(request, id):
+def education(request):
+    return render(request, "education.html")
+
+def projects(request):
+    projects = Project.objects.all()
+    tags = Tag.objects.all()
+    return render(request, "projects.html", {"projects": projects, "tags": tags})
+
+def projectView(request, id):
     project = get_object_or_404(Project, pk=id)
-    return render(request, "project.html", {"project": project})
+    return render(request, "project_view.html", {"projects": project})
+
+def contact(request):
+    return render(request, "contact.html")
